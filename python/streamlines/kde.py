Mean HSL azimuth, magnitude, arrow etc; fixed aspect masking, uv filter

python/streamlines/kde.py
@@ -77,7 +77,7 @@ def estimate_bivariate_pdf( distbn ):
                       distbn, action='histogram',  is_bivariate=True, 
                       sl_array=sl_array, verbose=verbose)
         
-    vprint(verbose,'kernel filtering rows..',end='')
+    vprint(verbose,'kernel filtering rows...',end='')
     # PDF
     cl_kernel_fn = 'pdf_bivariate_rows'
     partial_pdf_array \
@@ -164,7 +164,7 @@ def estimate_univariate_pdf( distbn, do_detrend=False, logx_vec=None):
         pdf *= np.sum(histogram_array)
         histogram_array = pdf.copy().astype(np.uint32)
         
-    vprint(verbose,'kernel filtering..',end='')
+    vprint(verbose,'kernel filtering...',end='')
     # PDF
     cl_kernel_fn = 'pdf_univariate'
     pdf_array \

python/streamlines/mapping.py
@@ -4,9 +4,11 @@ TBD
 
 import numpy  as np
 import pandas as pd
-from skimage.morphology   import skeletonize, thin, medial_axis, disk
-from skimage.filters      import gaussian
-from skimage.filters.rank import mean,median
+from cmath import rect, polar
+from sklearn.preprocessing import normalize
+from skimage.morphology    import skeletonize, thin, medial_axis, disk
+from skimage.filters       import gaussian
+from skimage.filters.rank  import mean,median
 from scipy.ndimage            import gaussian_filter
 from scipy.ndimage.morphology import binary_fill_holes
 from scipy.ndimage.morphology import binary_dilation,generate_binary_structure
@@ -48,11 +50,11 @@ class Mapping(Core):
         """
         self.print('\n**Mapping begin**') 
         self.prepare()
-        self.map_segments_channels()
-        self.map_hsl_ridges_midslopes_aspect()
+        self.mapping_segments_channels()
+        self.mapping_hsl_ridges_midslopes_aspect()
         self.print('**Mapping end**\n')  
                 
-    def map_segments_channels(self):
+    def mapping_segments_channels(self):
         """
         TBD.
         """
@@ -93,7 +95,7 @@ class Mapping(Core):
 
         self.print('**Mapping segments and channels end**\n')  
         
-    def map_hsl_ridges_midslopes_aspect(self):
+    def mapping_hsl_ridges_midslopes_aspect(self):
         """
         TBD.
         """
@@ -119,6 +121,7 @@ class Mapping(Core):
         
         self.print('**Mapping HSL, ridges, midslopes, aspect end**\n')  
         
+
     def prepare(self):
         self.print('Preparing...',end='')  
         try:
@@ -371,9 +374,10 @@ class Mapping(Core):
         sys.stdout.flush()
 
         slope_threshold = self.aspect_slope_threshold
-        median_radius   = self.aspect_median_filter_radius
+        median_radius   = self.aspect_median_filter_radius/self.geodata.pixel_size
         slope_array     = self.preprocess.slope_array.copy()
         uv_array        = self.preprocess.uv_array.copy()
+        uv_filter_width = self.uv_filter_width/self.geodata.pixel_size
         is_channel      = self.info.is_channel
         mapping_array   = self.data.mapping_array
         mask_array      = np.zeros_like(mapping_array, dtype=np.bool)
@@ -382,7 +386,6 @@ class Mapping(Core):
             sf = np.max(slope_array)/255.0
             median_slope_array = sf*median(np.uint8(slope_array/sf),disk(median_radius))
             slope_array = median_slope_array
-        uv_filter_width = 3      
         uv_array[:,:,0] = gaussian_filter(uv_array[:,:,0],uv_filter_width)
         uv_array[:,:,1] = gaussian_filter(uv_array[:,:,1],uv_filter_width)
         mask_array[  (slope_array<slope_threshold)
@@ -391,44 +394,47 @@ class Mapping(Core):
                  np.arctan2(uv_array[:,:,1],uv_array[:,:,0]), mask=mask_array )
         self.print('done')  
                                                          
-    def compute_hsl_aspect(self, n_bins=None):
+    def compute_hsl_aspect(self, n_bins=None, hsl_averaging_threshold=None):
         self.print('Computing hillslope length-aspect function...',end='',flush=True)
         sys.stdout.flush()
         
-#         aspect_range = np.pi
-        aspect_range = 180
         if n_bins is None:
             n_bins = 60
-        pad = self.geodata.pad_width
-#         self.hsl_smoothed_array[75:150,:]=0
-#         self.hsl_smoothed_array[self.hsl_smoothed_array<120]=0
-        aspect_array = np.rad2deg(self.aspect_array[pad:-pad,pad:-pad].copy())
-        hsl_array = self.hsl_smoothed_array.copy()
-
-        aspect_array = aspect_array[hsl_array>0.0]
-        hsl_array    = hsl_array[hsl_array>0.0]
-        hsl_array    = hsl_array[np.abs(aspect_array)>0.0]
-        aspect_array = aspect_array[np.abs(aspect_array)>0.0]
-                
-        mask_array   = np.ma.getmaskarray(aspect_array)| np.ma.getmaskarray(hsl_array)
-        hsl_aspect_array = np.stack(
-                       (np.ma.masked_array(hsl_array,    mask=mask_array).ravel(),
-                        np.ma.masked_array(aspect_array, mask=mask_array).ravel()),
-                        axis=1)
+        if hsl_averaging_threshold is None:
+            hsl_averaging_threshold = self.hsl_averaging_threshold
+        
+        pad           = self.geodata.pad_width
+        mask_array    = self.data.mask_array[pad:-pad,pad:-pad]
+        aspect_array  = self.aspect_array[pad:-pad,pad:-pad].copy()[~mask_array]
+        hsl_array     = self.hsl_smoothed_array.copy()[~mask_array]
+        aspect_array  = np.rad2deg(aspect_array[hsl_array>hsl_averaging_threshold])
+        hsl_array     = hsl_array[hsl_array>hsl_averaging_threshold]
+        hsl_aspect_array = np.stack( (hsl_array,aspect_array), axis=1)
+        
         # Sort in-place using column 1 (aspect) as key
         self.hsl_aspect_array = hsl_aspect_array[hsl_aspect_array[:,1].argsort()]
-        self.hsl_aspect_df = pd.DataFrame(data=self.hsl_aspect_array,
-                                          columns=['hsl','aspect'])
-        half_bin = aspect_range/n_bins
-        bins = np.linspace(-aspect_range,+aspect_range+2*half_bin,n_bins+2)-half_bin
-
+        self.hsl_aspect_df    = pd.DataFrame(data=self.hsl_aspect_array,
+                                             columns=['hsl','aspect'])
+        half_bin_width = 180/n_bins
+        bin_width = half_bin_width*2
+        bins = np.linspace(-180,+180+bin_width,n_bins+2)-half_bin_width
         self.hsl_aspect_df['groups'] = pd.cut(self.hsl_aspect_df['aspect'], bins)
         self.hsl_aspect_averages = self.hsl_aspect_df.groupby('groups')['hsl'].mean()
-        bins = np.deg2rad(bins[:-1]+half_bin)
+        bins = np.deg2rad(bins[:-1]+half_bin_width)
         hsl = self.hsl_aspect_averages.values
         south_hsl = (hsl[0]+hsl[-1])/2.0
-        hsl[0] = south_hsl
+        hsl[0]  = south_hsl
         hsl[-1] = south_hsl
         self.hsl_aspect_averages_array = np.stack((hsl,bins),axis=1)
+        
+        complex_hsl_vector_array \
+            = (np.array([rect(ha[0],ha[1]) 
+                         for ha in self.hsl_aspect_averages_array]))
+        mean_complex_hsl_vector \
+            = np.mean(complex_hsl_vector_array[~np.isnan(complex_hsl_vector_array)])
+        mhsl = polar(mean_complex_hsl_vector)
+        self.mean_hsl_magnitude = mhsl[0]
+        self.mean_hsl_azimuth = np.rad2deg(mhsl[1])
+            
         self.print('done')  
                                                          

python/streamlines/plot.py
@@ -19,8 +19,9 @@ from   matplotlib import streamplot
 import matplotlib.pyplot   as plt
 import matplotlib.gridspec as gridspec
 import matplotlib.ticker   as ticker
-from matplotlib.pyplot import streamplot
-from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.pyplot  import streamplot
+from matplotlib.colors  import LinearSegmentedColormap
+from matplotlib.patches import ArrowStyle, FancyArrowPatch
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import colorsys
 from scipy.stats   import norm
@@ -410,7 +411,7 @@ class Plot(Core):
             c_interval *= 2        
         bands = np.arange(0,c_max+2*c_interval,c_interval).astype(np.uint32)
         band_labels = ['{}m'.format(band) for band in bands]
-        axes.set_rgrids(bands, labels=band_labels, color='b',style=None)
+        axes.set_rgrids(bands, labels=band_labels, color='blue',style=None)
         angles = np.arange(0,360,45).astype(np.uint32)
         spc = u'\N{space}'
         angle_labels = [r'0$\degree$',r'45$\degree$',
@@ -421,6 +422,21 @@ class Plot(Core):
 #         axes.tick_params(pad=8)
         axes.grid(color='b',alpha=0.5,linestyle='dashed')
         
+        mha = np.deg2rad(self.mapping.mean_hsl_azimuth)
+        mhm = self.mapping.mean_hsl_magnitude
+        mhl = z_max/2.0
+        head_length = 40   # hack - how to correctly scale??
+        arrow_style = ArrowStyle.Fancy(head_length=head_length,head_width=head_length/2,
+                                      tail_width=1)
+        arrow_patch = FancyArrowPatch((mha-np.pi,mhl/3.0), (mha,mhl),
+                                      shrinkA=1, shrinkB=1,
+                                      arrowstyle=arrow_style,
+                                      facecolor='blue', edgecolor='darkblue', 
+                                      linewidth=4)
+        axes.add_patch(arrow_patch)
+        axes.plot(mha,mhm,'.',ms=40,color='blue')
+        axes.plot(mha,mhm,'.',ms=30,color='lightblue')
+                      
         self._force_display(fig)
         self._record_fig(fig_name,fig)
 