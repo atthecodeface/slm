Fix dilated bbox issue, copyto bug. Bboxed HSL works wout basin masking

python/streamlines/mapping.py
@@ -73,7 +73,7 @@ class Mapping(Core):
 
     def pass1(self):
         vprint(self.vprogress,'\n**Pass#1 begin**')
-        self._switch_to_quiet_mode()
+#         self._switch_to_quiet_mode()
         # Shorthand
         pad = self.geodata.pad_width
         nxp = self.geodata.roi_nx+pad*2
@@ -118,33 +118,43 @@ class Mapping(Core):
         self.coarse_subsegments   = np.sort(coarse_subsegments[coarse_subsegments!=0])
         self.n_coarse_subsegments = self.coarse_subsegments.shape[0]
         # Make a mask to select all coarse subsegments
+        pdebug('merged_coarse_mask',self.merged_coarse_mask[self.merged_coarse_mask==False].shape)
+        pdebug('coarse_subsegment_array',self.coarse_subsegment_array[self.coarse_subsegment_array!=0].shape)
         for label in self.coarse_subsegments:
+            pdebug('merging',label)
             self.merged_coarse_mask[self.coarse_subsegment_array==label] = False
+            pdebug(self.merged_coarse_mask[self.merged_coarse_mask==False].shape)
         # Copy the coarse subsegments so they can be readily visualized
         self.label_array = self.coarse_subsegment_array.copy()
-        self._switch_back_to_verbose_mode()
+#         self._switch_back_to_verbose_mode()
         vprint(self.vprogress,'**Pass#1 end**') 
 
     def make_coarse_subsegment_masks(self, coarse_subsegment, is_left_or_right,
                                      raw_mask=None, dilated_mask=None):
-        # Start with inverted raw mask
-        raw_mask.fill(False)
-        raw_mask[self.coarse_subsegment_array==coarse_subsegment] = True
-        # Dilate mask by 2 if left or 1 if right flank coarse subsegment
-        #   - left dilation needs to encompass bordering right-flank channel pixels
+        # Initialize raw mask with masked everywhere
+        raw_mask.fill(True)
+        # Unmask this coarse segment
+        raw_mask[self.coarse_subsegment_array==coarse_subsegment] = False
+        # Dilate this coarse segment mask by 2 if left or 1 if right flank
+        #   - cos left dilation needs to grow to cover right-flank channel pixels
         n = (2 if is_left_or_right=='left' else 1)
-        # Convert to normal raw mask where True => masked off - do in-situ
-        dilate(raw_mask,n_iterations=n, out=dilated_mask)
+        dilate(~raw_mask,n_iterations=n, out=dilated_mask)
         np.invert(dilated_mask, out=dilated_mask)
-        # Now invert the raw mask as well - do in-situ
-        np.invert(raw_mask, out=raw_mask)
+        # Shorthand
         pad = self.geodata.pad_width
-#         padded_mask_array = np.pad(dilated_mask, (pad,pad),
-#                                    'constant', constant_values=(True,True))
+        # Ensure dilated unmask does not encroach on boundary mask
+        dilated_mask[:pad,:]  = True
+        dilated_mask[:,:pad]  = True
+        dilated_mask[-pad:,:] = True
+        dilated_mask[:,-pad:] = True
         # Define bbox
-        bbox_dilated_segment, nx,ny = get_bbox(~dilated_mask)
-        self.print('Dilated subsegment mask bbox = {}'.format(bbox_dilated_segment))
-        return bbox_dilated_segment
+        bbox_raw_mask, nx,ny = get_bbox(~raw_mask)
+        bbox_dilated_mask, nxd,nyd = get_bbox(~dilated_mask)
+        pdebug('bbox_raw_mask',bbox_raw_mask,'bbox_dilated_mask',bbox_dilated_mask)
+        pdebug('raw_mask',raw_mask.shape,'dilated_mask',dilated_mask.shape)
+        pad = self.geodata.pad_width
+#         self.print('Dilated subsegment mask bbox = {}'.format(bbox_dilated_mask))
+        return bbox_dilated_mask
 
     def pass2(self):
         vprint(self.vprogress,'\n**Pass#2 begin**') 
@@ -161,19 +171,21 @@ class Mapping(Core):
         self.state.add_active_mask({'merged_coarse': self.merged_coarse_mask})
         # Initialize the full ROI-scale HSL grid and a buffer
         # Also a mapping array for channels etc
-        raw_mask_array          = np.zeros((nxp,nyp), dtype=np.bool)
-        dilated_mask_array      = np.zeros((nxp,nyp), dtype=np.bool)
-        merged_mask_array       = np.zeros((nxp,nyp), dtype=np.bool)
-        self.mapping_array      = np.zeros((nxp,nyp), dtype=np.uint32)
-        self.hsl_array          = np.zeros((nxp,nyp), dtype=np.float32)
-        tmp_hsl_array           = self.hsl_array.copy()  
-#         mask_array              = self.mask_array
+        raw_mask_array     = np.zeros((nxp,nyp), dtype=np.bool)
+        pdebug('self.geodata.roi_nx,ny = ',(self.geodata.roi_nx,self.geodata.roi_ny))
+        pdebug('nxp,nyp = ',(nxp,nyp))
+        pdebug('raw_mask_array',raw_mask_array.shape)
+        dilated_mask_array = np.zeros((nxp,nyp), dtype=np.bool)
+        merged_mask_array  = np.zeros((nxp,nyp), dtype=np.bool)
+        self.mapping_array = np.zeros((nxp,nyp), dtype=np.uint32)
+        self.hsl_array     = np.zeros((nxp,nyp), dtype=np.float32)
+        tmp_hsl_array      = self.hsl_array.copy()  
+#         mask_array         = self.mask_array
         # Iterate over the coarse subsegments
         for idx,coarse_subsegment in enumerate(self.coarse_subsegments):
-#             self.mask_array = np.zeros((nxp,nyp), dtype=np.bool)
+#         for idx,coarse_subsegment in enumerate([71]):
             if idx>0:
                 merged_mask_array.fill(False)
-#         for idx,coarse_subsegment in enumerate([71]):
             # Report % progress
             self.report_progress(idx, n_segments)
             # Create metadata container for this coarse subsegment
@@ -194,7 +206,7 @@ class Mapping(Core):
                        .format(coarse_subsegment,idx+1,n_segments,is_left_or_right))
             # Deploy the dilated coarse-subsegment mask
             self.state.add_active_mask({'dilated_segment': dilated_mask_array})
-            self.state.merge_active_masks(mask=merged_mask_array)
+            self.state.merge_active_masks(out=merged_mask_array)
             self.print('Dilated coarse subsegment mask bounding box: {}'.format(bbox))
             
             bbox, bnx, bny = get_bbox(~merged_mask_array)
@@ -208,6 +220,7 @@ class Mapping(Core):
                          slc_array     = self.trace.slc_array,
                          slt_array     = self.trace.slt_array )
 
+            pdebug(merged_mask_array.shape, info.nx_padded,info.ny_padded)
             # Compute slt pdf and estimate channel threshold from it
             channel_threshold \
                 = self.analysis.estimate_channel_threshold(data, verbose=self.vbackup)
@@ -240,7 +253,7 @@ class Mapping(Core):
             # Deploy this iteration's raw coarse-subsegment mask
             self.state.add_active_mask({'raw_segment': raw_mask_array})
             bounds = data.bounds_grid
-            self.state.merge_active_masks(mask=merged_mask_array)
+            self.state.merge_active_masks(out=merged_mask_array)
             tmp_hsl_array.fill(0.0)
             tmp_hsl_array[bounds] = data.hsl_array
             tmp_hsl_array[np.isnan(tmp_hsl_array)] = 0.0
@@ -422,7 +435,7 @@ class Mapping(Core):
                  ((data.mapping_array & flag)>0) & (~data.mask_array)
                                     ].astype(np.int32)
         unique_labels         = np.unique(data.traj_label_array)
-        self.hillslope_labels = unique_labels[unique_labels!=0].astype(np.int32)
+        data.hillslope_labels = unique_labels[unique_labels!=0].astype(np.int32)
         self.n_subsegments    = unique_labels.shape[0]
         self.print('selected {}'.format(self.n_subsegments))
         self.print('...done')  
@@ -439,7 +452,7 @@ class Mapping(Core):
         self.hsl_df = df
         
         try:
-            stats_df = pd.DataFrame(self.hillslope_labels,columns=['label'])
+            stats_df = pd.DataFrame(data.hillslope_labels,columns=['label'])
             stats_list = ( ('count','count'), ('mean','mean [m]'), ('std','stddev [m]') )
             for stat in stats_list:
                 stats_df = stats_df.join( getattr(df.groupby('label'),stat[0])() ,on='label')

python/streamlines/plot.py
@@ -459,9 +459,14 @@ class Plot(Core):
         fig_name='label'
         window_title='label'     
         tmp_array = (self.mapping.label_array.copy().astype(np.int32))
+#         pdebug('label', tmp_array.shape,np.unique(tmp_array))
+#         tmp_array[tmp_array!=1]=0
+        mask_array = self.state.merge_active_masks()
+#         pdebug('list_active_masks',self.state.list_active_masks())
         self.plot_gridded_data(tmp_array,
                                'randomized', 
                                fig_name=fig_name, window_size_factor=window_size_factor,
+                               mask_array=mask_array,
                                window_title=window_title,
                                do_shaded_relief=False, 
                                do_flip_cmap=False, do_balance_cmap=False)
@@ -495,9 +500,10 @@ class Plot(Core):
             else:
                 z_max = self.hsl_z_max
         grid_array = np.clip(self.mapping.hsl_array.copy(),z_min,z_max)
-        mask_array = np.zeros_like(grid_array).astype(np.bool)
-        # Hack - need to rebuild label_array during multipass         
+#         mask_array = np.zeros_like(grid_array).astype(np.bool)
 #         mask_array[self.mapping.label_array==0] = True
+        mask_array = self.state.merge_active_masks()
+#         pdebug('list_active_masks',self.state.list_active_masks())
         self.plot_gridded_data(grid_array,
                                cmap,  # rainbow
                                mask_array=mask_array,
@@ -557,6 +563,8 @@ class Plot(Core):
             linewidth = self.contour_hsl_linewidth
                 
         mask_array = self.state.merge_active_masks()[pad:-pad,pad:-pad]
+#         pdebug('list_active_masks',self.state.list_active_masks())
+#         mask_array &= False
         if do_shaded_relief:
             hillshade_alpha = self.grid_shaded_relief_hillshade_alpha
             hsl_alpha = 0.3
@@ -830,8 +838,7 @@ class Plot(Core):
         if mask_array is None:
             mask_array = np.zeros_like(grid_array).astype(np.bool)
         pad = self.geodata.pad_width     
-        mask_array = mask_array[pad:-pad,
-                                pad:-pad]
+        mask_array = mask_array[pad:-pad,pad:-pad]
         if self.geodata.do_basin_masking:
             mask_array |= self.state.merge_active_masks()[pad:-pad,pad:-pad]
             
@@ -857,9 +864,9 @@ class Plot(Core):
             grid_alpha = 1.0
 
         grid_array = grid_array[pad:-pad,pad:-pad]
-        extra_mask_array = grid_array.astype(np.bool)
-        mask_array = mask_array | (~extra_mask_array)
-#         mask_array &= 0
+#         extra_mask_array = grid_array.astype(np.bool)
+#         mask_array = mask_array | (~extra_mask_array)
+#         mask_array &= False
         masked_grid_array = np.ma.masked_array(grid_array, mask=mask_array)
         if do_flip_cmap:
             masked_grid_array = -masked_grid_array

python/streamlines/segment.py
@@ -190,7 +190,7 @@ def subsegment_flanks( cl_state, info, data, verbose ):
     cl_state.kernel_fn = 'fix_left_flanks'
     pocl.gpu_compute(cl_state, info, array_dict, info.verbose)
     
-    n_left_right_subsegments = np.unique(data.label_array).shape[0]
+    n_left_right_subsegments = np.unique(data.label_array[data.label_array!=0]).shape[0]
     vprint(verbose, ' -- number of left & right subsegments={}'
            .format(n_left_right_subsegments))
     

python/streamlines/state.py
@@ -313,6 +313,12 @@ class State(Core):
         if mask_name in self.active_masks_dict.keys():
             self.active_masks_dict.pop(mask_name)
         
+    def list_active_masks(self):
+        """
+        TBD
+        """ 
+        return self.active_masks_dict.keys()
+        
     def reset_active_masks(self):
         """
         TBD
@@ -324,7 +330,7 @@ class State(Core):
             = {k: self.active_masks_dict[k] for k in self.active_masks_dict 
                                             if  k in masks_keep_list}
         
-    def merge_active_masks(self, mask=None):
+    def merge_active_masks(self, out=None):
         """
         TBD
         """ 
@@ -333,13 +339,13 @@ class State(Core):
         # Create a mask from a blend of all those active
         for idx, mask_array in enumerate(self.active_masks_dict.values()):
             if idx==0:
-                if mask is None:
-                    active_mask_array = mask_array.copy()
+                if out is None:
+                    out = mask_array.copy()
                 else:
-                    active_mask_array = mask
-                    np.copyto(mask_array,mask)
+                    #  numpy.copyto(dst, src, casting='same_kind', where=True)
+                    np.copyto(out,mask_array)
             else:
-                active_mask_array |= mask_array
-        return active_mask_array
+                out |= mask_array
+        return out
         
                 

python/streamlines/useful.py
@@ -53,6 +53,7 @@ class Data():
         self.traj_stats_df = traj_stats_df
         self.bounds_grid = bounds_grid
         self.bounds_slx  = bounds_slx
+        self.hillslope_labels = None
 #     self.print('done')
 
 class Info():    
@@ -270,7 +271,7 @@ def vprint(verbose, *args, **kwargs):
         # Try to really force this line to print before the GPU prints anything
         sys.stdout.flush()
 
-def create_seeds(mask, pad_width, n_work_items, n_seed_points=None,
+def create_seeds(mask, pad, n_work_items, n_seed_points=None,
                  do_shuffle=True, rng_seed=1, verbose=False):
     """
     Generate seed points for tracing of streamline trajectories.   
@@ -281,9 +282,7 @@ def create_seeds(mask, pad_width, n_work_items, n_seed_points=None,
         int: n_padded_seed_points  
     """    
     vprint(verbose,'Generating seed points...', end='')
-    seed_point_array \
-        = ((np.argwhere(~mask).astype(np.float32) - pad_width)
-           ).copy()
+    seed_point_array = ((np.argwhere(~mask).astype(np.float32)-pad))
     # Randomize seed point sequence to help space out memory accesses by kernel instances
     if do_shuffle:
         vprint(verbose,'shuffling...', end='')
@@ -291,7 +290,7 @@ def create_seeds(mask, pad_width, n_work_items, n_seed_points=None,
         np.random.shuffle(seed_point_array)
     # Truncate if we only want to visualize a subset of streamlines across the DTM
     if n_seed_points is not None and n_seed_points>0:
-        seed_point_array = seed_point_array[:n_seed_points].copy().astype(np.float32)
+        seed_point_array = seed_point_array[:n_seed_points].astype(np.float32)
     else:
         n_seed_points = seed_point_array.shape[0]
 
@@ -304,7 +303,7 @@ def create_seeds(mask, pad_width, n_work_items, n_seed_points=None,
     else:
         vprint(verbose,'no padding needed...', end='')
     vprint(verbose,'done')
-    return seed_point_array, n_seed_points, n_padded_seed_points
+    return seed_point_array.copy(), n_seed_points, n_padded_seed_points
 
 def pick_seeds(mask=None, map=None, flag=None, pad=None):
     """
@@ -320,14 +319,12 @@ def pick_seeds(mask=None, map=None, flag=None, pad=None):
         numpy.ndarray: seed point array
     """
     if mask is None and map is not None:
-        seed_point_array \
-            = (np.argwhere((map & flag)>0).astype(np.float32)-pad).copy()
+        seed_point_array = (np.argwhere((map & flag)>0).astype(np.float32)-pad)
     elif mask is not None and map is None:
-        seed_point_array = (np.argwhere(~mask).astype(np.float32)-pad).copy()
+        seed_point_array = (np.argwhere(~mask).astype(np.float32)-pad)
     else:
-        seed_point_array \
-            = (np.argwhere(~mask & ((map & flag)>0)).astype(np.float32)-pad).copy()
-    return seed_point_array
+        seed_point_array = (np.argwhere(~mask & ((map & flag)>0)).astype(np.float32)-pad)
+    return seed_point_array.copy()
     
 def compute_stats(traj_length_array, traj_nsteps_array, pixel_size, verbose):
     """